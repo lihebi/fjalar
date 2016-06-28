a4705fb2578c87d923e2551a832d7b44704f29b4
Mark Roberts <markro@cs.washington.edu>
2016-06-28 15:24:09 -0700

    test branch for testing kvasir regressions

--- .travis-build.sh
+++ .travis-build.sh
@@ -15,23 +15,7 @@ ls -l /lib/x86_64-linux-gnu/libc-*
 # TODO: The tests ought to work even if $DAIKONDIR is not set.
 export DAIKONDIR=`pwd`/../daikon
 
-make build
+make ../daikon
 
-make doc
-
-## Valgrind doesn't pass its own tests ("make test").  So we should have a
-## version of the target that determines the current operating system,
-## compares the observed failures to the expected failures (those suffered
-## by "make test" on a fresh Valgrind installation on that OS), and the
-## overall target only fails if the set of failing tests is different.
-# make test
-
-## "make daikon-test" is disabled; see immediately below
-# make daikon-test
-## Kvasir does not currently pass all its tests on Ubuntu 14.04 which is
-## used by Travis.  Reinstate "make daikon-test" fails.  Reinstate "daikon-test"
-## as soon as we have improved Kvasir and/or its test suite.  In the meanwhile,
-# TEMP-daikon-test is a temporary substitute for daikon-test that runs
-# a subset of kvasir-tests.
-make TEMP-daikon-test
+make -C ../daikon compile daikon.jar kvasir
 
